d82b29df5210420997491389af356034aadb49d7
francis <u3096928@uni.canberra.edu.au>
2021-09-30 13:52:52 +1000

    make it work

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const request = require('request')
+const bodyParser = require('body-parser')
+const app = express()
+const fs = require('fs')
+
+require('dotenv').config()
+
+const api_key = process.env.API_KEY
+var places
+fs.readFile('places.json',(err,data)=>{
+    places = JSON.parse(data)
+    places.forEach((p)=>{
+        p.name=p.name.charAt(0).toUpperCase()+p.name.slice(1)
+    })
+})
+
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended:true}))
+app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+    var j = JSON.stringify(places)
+    
+    res.render('index',{places:j})
+})
+
+const threshold = 0.01
+app.get('/city',(req,res)=>{
+    let name = req.query.name
+            
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&appid=${api_key}`
+
+    request(url,(err,response,body)=>{
+        if (err){
+            res.send('error')
+        }else{
+            let weather = JSON.parse(body)
+            if (weather.main==undefined){
+                res.send('not found')
+            }else{
+                res.send(weather)
+            }
+        }
+    })
+    
+    
+    // res.end()
+})
+
+async function get_city_weather(city){
+
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api_key}`
+
+    request(url,(err,res,body)=>{
+        console.log(res)
+        if (err){
+            return "error"
+        }else{
+            let weather = JSON.parse(body)
+            console.log(weather)
+            return weather
+
+            if (weather.main==null){
+                res.render('index',{weather:null,error:"Error"})
+            }else{
+                let place = `${weather.name}, ${weather.sys.country}`
+                let timezone = `${new Date(weather.dt - weather.timezone)}`
+            }
+        }
+
+    })
+}
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log('now listening on',process.env.PORT)
+})
